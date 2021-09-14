9fb53d451f14824d7f8edac1fad9a5f12138f7e3
Ansh <anshbhatia123@gmail.com>
2021-09-14 17:32:40 +0530

    loginpageui Day 4

--- flutter/flutter_complete_guide/lib/pages/login_page.dart
+++ flutter/flutter_complete_guide/lib/pages/login_page.dart
@@ -4,17 +4,43 @@ class Loginpage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Login Page"),
-      ),
-      body: Center(
-        child: Text("Login Page",style: TextStyle(
-          fontSize: 40,
-          color: Colors.blue
-        )),
-      ),
-      drawer: Drawer(),
+    return Material(
+      color: Colors.white,
+     child: Column(
+       children: [
+         Image.asset("assets/login_img.png",fit: BoxFit.cover,),
+         SizedBox(height: 20,),
+       Text("Login to Continue",style: TextStyle(
+         fontSize: 40,
+         color: Colors.purple
+       ),),
+       SizedBox(height: 20,),
+       Padding(
+         padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+         child: Column(
+           children: [
+             TextFormField(
+           decoration: InputDecoration(
+             hintText: ("Username"),
+             label: Text("Enter Username")
+           ),
+         ),
+         TextFormField(
+           obscureText: true,
+           decoration: InputDecoration(
+             hintText: ("Password"),
+             label: Text("Enter Password")
+           ),
+         ),
+         SizedBox(height: 20,),
+           ElevatedButton(child: Text("Login"),onPressed: (){
+             print("Working");
+           },)
+           ],
+         ),
+       )
+       ],
+     )
     );
   }
 }
